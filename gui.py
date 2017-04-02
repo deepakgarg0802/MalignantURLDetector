db7384d6a60507988b14aa12130fa2c381b6bd9f
deepakgarg0802 <deepakgarg0802@gmail.com>
2017-04-02 18:39:24 +0530

    Feat : Caching mechanism added Style : Title bar of tkinter box changed Add : Blank cache file added

--- gui.py
+++ gui.py
@@ -6,6 +6,7 @@ import main
 
 root = Tk()
 frame = Frame(root)
+root.title("Malignant URL Checker")
 frame.pack()
 bottomframe = Frame(root)
 bottomframe.pack( side = BOTTOM )
@@ -17,16 +18,39 @@ E1.pack(side = RIGHT)
 
 def submitCallBack():
 	url=E1.get()
-	main.features_to_csv(url,'test_features.csv')
-	return_ans = tr.train_and_test('url_features.csv','test_features.csv')
-	a=str(return_ans).split()
-	if int(a[1])==0:
-		tkMessageBox.showinfo( "URL Checker Result","The URL "+url+" is Benign")
-	elif int(a[1])==1:
+
+	flag=main.check_cache(url,'mycache.csv')
+
+	if flag==0:
+			tkMessageBox.showinfo( "URL Checker Result","The URL "+url+" is Benign")
+
+	elif flag==1:
 		tkMessageBox.showinfo( "URL Checker Result","The URL "+url+" is Malicious")
-	else:
+	
+	elif flag==2:
 		tkMessageBox.showinfo( "URL Checker Result","The URL "+url+" is Malware")
-   		   
+
+	else :
+
+		main.features_to_csv(url,'test_features.csv')
+		return_ans = tr.train_and_test('url_features.csv','test_features.csv')
+		a=str(return_ans).split()
+
+		if int(a[1])==0:
+			flag=0
+			main.append_result([url,flag],'mycache.csv')
+			tkMessageBox.showinfo( "URL Checker Result","The URL "+url+" is Benign")
+
+		elif int(a[1])==1:
+			flag=1
+			append_result([url,flag],'mycache.csv')
+			tkMessageBox.showinfo( "URL Checker Result","The URL "+url+" is Malicious")
+		else:
+			flag=2
+			append_result([url,flag],'mycache.csv')
+			tkMessageBox.showinfo( "URL Checker Result","The URL "+url+" is Malware")
+
+	   		   
 B1 = Button(bottomframe, text ="Submit", command = submitCallBack)
 
 B1.pack()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import Feature_extraction as urlfeature
 import trainer as tr
+import pandas
 
 #---------function to write the attrivutes to csv file--------------#
 def resultwriter(feature,output_dest):
@@ -22,3 +23,20 @@ def features_to_csv(url,output_dest):
         ret_dict=urlfeature.feature_extract(url)
         feature.append([url,ret_dict]);
     resultwriter(feature,output_dest)
+
+#---------function to append the result to csv file--------------#
+def check_cache(input_url,output_dest):
+    flag=-1
+    data = pandas.DataFrame.from_csv(output_dest, index_col=None)
+    if any(data.URL==input_url):
+        flag=data[data.URL==input_url].malicious.values[0]
+        
+    return flag
+
+#---------function to append the result to csv file--------------#
+def append_result(outcome,output_dest):
+    with open(output_dest,'a') as f:
+        fieldnames = ['URL', 'malicious']
+        w = csv.DictWriter(f,fieldnames=fieldnames)
+        #w.writeheader()
+        w.writerow({'URL' :outcome[0], 'malicious':outcome[1]})
